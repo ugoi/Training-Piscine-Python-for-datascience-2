25ef7d3538108d2fd5f0c9a06f737d8e40540e1d
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-09-25 20:12:00 +0200

    fix(ex01): convert year to int

--- ex01/aff_life.py
+++ ex01/aff_life.py
@@ -7,12 +7,13 @@ def main():
     try:
         df = load("life_expectancy_years.csv")
         germany = df[df['country'] == 'Germany']
-        life_expectancy_germany = germany.iloc[0, 1:]
-        plt.plot(life_expectancy_germany, color='blue')
+        life_expectancy_germany = germany.iloc[0, 1:].squeeze()
+        plt.plot(life_expectancy_germany.index.astype(int),
+                 life_expectancy_germany.values,
+                 color='blue')
         plt.title('Germany Life expectancy Projections')
         plt.xlabel('Year')
         plt.ylabel('Life expectancy')
-        plt.xticks(life_expectancy_germany.index[::40])
         plt.tight_layout()
         plt.show()
     except Exception as e:
